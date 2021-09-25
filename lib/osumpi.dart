56c146db0aa430130694afe7ea61871a4fd6816c
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-25 22:08:07 +0530

    dropped using arrows

--- lib/osumpi.dart
+++ lib/osumpi.dart
@@ -24,73 +24,77 @@ mixin _AppThemeManager on ChangeNotifier {
     }
   }
 
-  ThemeData get theme => ThemeData(
-        brightness: Brightness.light,
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFF00B173),
-        canvasColor: const Color(0xFFF9FAFD),
-        textTheme: _defaultTextTheme
-            .apply(
-              bodyColor: Colors.black,
-              displayColor: Colors.black,
-            )
-            .copyWith(
-              headline6: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+  ThemeData get theme {
+    return ThemeData(
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: Colors.white,
+      primaryColor: const Color(0xFF00B173),
+      canvasColor: const Color(0xFFF9FAFD),
+      textTheme: _defaultTextTheme
+          .apply(
+            bodyColor: Colors.black,
+            displayColor: Colors.black,
+          )
+          .copyWith(
+            headline6: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-        shadowColor: Colors.blueGrey.shade50,
-        tooltipTheme: TooltipThemeData(
-          margin: const EdgeInsets.only(left: 50),
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors.black45,
           ),
-          verticalOffset: -10,
+      shadowColor: Colors.blueGrey.shade50,
+      tooltipTheme: TooltipThemeData(
+        margin: const EdgeInsets.only(left: 50),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.black45,
         ),
-        navigationRailTheme: NavigationRailThemeData(
-          backgroundColor: Colors.grey.shade50,
-          selectedIconTheme: const IconThemeData(
-            color: Colors.black,
-          ),
-          unselectedIconTheme: IconThemeData(
-            color: Colors.grey.shade500,
-          ),
+        verticalOffset: -10,
+      ),
+      navigationRailTheme: NavigationRailThemeData(
+        backgroundColor: Colors.grey.shade50,
+        selectedIconTheme: const IconThemeData(
+          color: Colors.black,
         ),
-        visualDensity: VisualDensity.comfortable,
-      );
+        unselectedIconTheme: IconThemeData(
+          color: Colors.grey.shade500,
+        ),
+      ),
+      visualDensity: VisualDensity.comfortable,
+    );
+  }
 
-  ThemeData get darkTheme => ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF292934),
-        backgroundColor: const Color(0xFF212228),
-        primaryColor: const Color(0xFF00B173),
-        accentColor: Colors.grey.shade600,
-        iconTheme: IconThemeData(
+  ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: const Color(0xFF292934),
+      backgroundColor: const Color(0xFF212228),
+      primaryColor: const Color(0xFF00B173),
+      accentColor: Colors.grey.shade600,
+      iconTheme: IconThemeData(
+        color: Colors.grey.shade600,
+      ),
+      textTheme: _defaultTextTheme.apply(
+        bodyColor: Colors.grey.shade300,
+        displayColor: Colors.grey.shade600,
+      ),
+      tooltipTheme: TooltipThemeData(
+        margin: const EdgeInsets.only(left: 50),
+        padding: const EdgeInsets.all(8),
+        // decoration: BoxDecoration(
+        //   // color: Colors.black45,
+        // ),
+        verticalOffset: -10,
+      ),
+      navigationRailTheme: NavigationRailThemeData(
+        backgroundColor: const Color(0xFF24242C),
+        selectedIconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        unselectedIconTheme: IconThemeData(
           color: Colors.grey.shade600,
         ),
-        textTheme: _defaultTextTheme.apply(
-          bodyColor: Colors.grey.shade300,
-          displayColor: Colors.grey.shade600,
-        ),
-        tooltipTheme: TooltipThemeData(
-          margin: const EdgeInsets.only(left: 50),
-          padding: const EdgeInsets.all(8),
-          // decoration: BoxDecoration(
-          //   // color: Colors.black45,
-          // ),
-          verticalOffset: -10,
-        ),
-        navigationRailTheme: NavigationRailThemeData(
-          backgroundColor: const Color(0xFF24242C),
-          selectedIconTheme: const IconThemeData(
-            color: Colors.white,
-          ),
-          unselectedIconTheme: IconThemeData(
-            color: Colors.grey.shade600,
-          ),
-        ),
-      );
+      ),
+    );
+  }
 
   TextTheme get _defaultTextTheme => GoogleFonts.openSansTextTheme();
 }
